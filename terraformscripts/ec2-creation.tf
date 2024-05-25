9f8b4f809334126960ff7a3ca5873ced7543900c
pinnacloud <158473484+pinnacloud@users.noreply.github.com>
2024-05-25 14:18:27 +0100

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,13 +1,14 @@
-provider "aws"{
-region="us-east-1"
+provider "aws" {
+  region = "us-east-1"
 }
 
-resource "aws_instance" "AWSEC2"{
-ami = "ami-0fe630eb857a6ec83"
-instance_type = "t2.micro"
-key_name= "linux"
-security_groups = ["launch-wizard-2"]
-tags = {
-Name = "tomcatservers"
-}
+resource "aws_instance" "ec2instance" {
+    ami = "ami-0fe630eb857a6ec83"
+    instance_type = "t2.micro"
+    key_name = "nd"
+    security_groups = [ "launch-wizard-2" ]
+    tags = {
+      Name = "tomcatservers"
+    }
+  
 }
