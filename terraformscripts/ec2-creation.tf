540dbe235dce8788aa82b144e9d95f390479d40c
pinnacloud <158473484+pinnacloud@users.noreply.github.com>
2024-05-25 01:38:44 +0100

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -3,7 +3,7 @@ region="us-east-1"
 }
 
 resource "aws_instance" "AWSEC2"{
-ami = "ami-001843b876406202a"
+ami = "ami-0fe630eb857a6ec83"
 instance_type = "t2.micro"
 key_name= "linux"
 security_groups = ["launch-wizard-2"]
